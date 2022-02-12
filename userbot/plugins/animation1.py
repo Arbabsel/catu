151d6252451215b0bf36a3d182874ab669ed4908
mohammadbotcat <99530265+mohammadbotcat@users.noreply.github.com>
2022-02-12 02:27:14 -0800

    Update animation1.py

--- userbot/plugins/animation1.py
+++ userbot/plugins/animation1.py
@@ -161,7 +161,7 @@ async def _(event):
         "What The",
         "What The F",
         "What The F Brah",
-        "What The F Brah[ㅤㅤㅤㅤㅤㅤ\u2063](https://telegra.ph/file/f3b760e4a99340d331f9b.jpg)",
+        "What The F Brah[ㅤㅤㅤㅤㅤㅤ\u2063](https://telegra.ph/file/a9b406c3e85aff98ec7f1.jpg)",
     ]
     for i in animation_ttl:
         await asyncio.sleep(animation_interval)
